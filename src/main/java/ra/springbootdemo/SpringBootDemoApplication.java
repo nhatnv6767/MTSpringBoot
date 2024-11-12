65769dd69f3374bd8b6e9eb0860c0856d1868cf9
bhnone <>
2024-11-13 01:11:56 +0900

    5

--- src/main/java/ra/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/ra/springbootdemo/SpringBootDemoApplication.java
@@ -8,10 +8,7 @@ public class SpringBootDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDemoApplication.class, args);
-        ///System.out.println("Hello World");
-
         
-        //
     }
 
 }
