cea423ddad049720d6dcfac67f0dcaa2b10e160a
bhnone <>
2024-11-13 01:26:43 +0900

    7

--- src/main/java/ra/mtspringboot3/MtSpringBoot3Application.java
+++ src/main/java/ra/mtspringboot3/MtSpringBoot3Application.java
@@ -8,7 +8,7 @@ public class MtSpringBoot3Application {
 
     public static void main(String[] args) {
         SpringApplication.run(MtSpringBoot3Application.class, args);
-        // main -> master
+        // nut -> master
     }
 
 }
