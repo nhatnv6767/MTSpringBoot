c7680359587041e49e133b3f12706fe74c3814eb
bhnone <>
2024-11-13 00:56:06 +0900

    why dont increase

--- src/main/java/ra/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/ra/springbootdemo/SpringBootDemoApplication.java
@@ -9,6 +9,7 @@ public class SpringBootDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDemoApplication.class, args);
         ///System.out.println("Hello World");
+        //
     }
 
 }
