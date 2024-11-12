1ef3761ada4e052ff23e0cc00f20392557c16430
bhnone <>
2024-11-13 01:18:32 +0900

    6

---
+++ src/main/java/ra/mtspringboot3/MtSpringBoot3Application.java
@@ -0,0 +1,13 @@
+package ra.mtspringboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtSpringBoot3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MtSpringBoot3Application.class, args);
+    }
+
+}
---
+++ src/test/java/ra/mtspringboot3/MtSpringBoot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ra.mtspringboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtSpringBoot3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
